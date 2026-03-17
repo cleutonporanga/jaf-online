9a313732c91609b44eb681d4856adf82701e4db3
Cleuton Lima <cleutonporanga@gmail.com>
2026-03-17 03:53:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5568459248-77c1f",
+  "appId": "1:769522566412:web:5ceb54316cf2adf5f63529",
+  "apiKey": "AIzaSyD0_OPqxKkIxgcIUX7OhxV9L6dNC4gCENg",
+  "authDomain": "studio-5568459248-77c1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "769522566412"
+};
